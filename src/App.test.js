cc32a35972c44a29321b345d850e7b0a7e6c8d66
MirekSz <miro1994>
2018-09-05 19:12:55 +0200

    test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Parent, MChild } from './App';
-
-
+import { render, fireEvent, cleanup, waitForElement, prettyDOM } from 'react-testing-library'
+afterEach(cleanup)
 it('renders without crashing parent', () => {
   const div = document.createElement('div');
   ReactDOM.render(<Parent />, div);
@@ -14,4 +14,22 @@ it('renders without crashing child', () => {
   const div = document.createElement('div');
   ReactDOM.render(<MChild a={{}} handler={handler} />, div);
   ReactDOM.unmountComponentAtNode(div);
+});
+class Parent extends React.Component {
+  render() {
+    return (<MChild a={model} handler={handler} />)
+  }
+}
+it('renders MCHild and call handler', () => {
+  let model = { i: 0 };
+  let handler = () => { model.i = 1; };
+  let { getByText, getByTestId, container, debug, rerender } = render(<MChild a={model} handler={handler} />);
+
+  expect(prettyDOM(container)).toMatch('"i":0');
+  fireEvent.click(getByText('a'));
+
+  let z = render(<MChild a={model} handler={handler} />);
+  // console.log(prettyDOM(container))
+  expect(prettyDOM(z.container)).toMatch('"i":1');
+  debug();;
 });
